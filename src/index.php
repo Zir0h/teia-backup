2e0c38e62f99ee73bde9402d07e623b3150c5404
Yannick Goossens <yannick@ziroh.be>
2024-02-10 10:41:19 +0100

    try to make the thing less ugly

--- src/index.php
+++ src/index.php
@@ -142,28 +142,49 @@ if ($response === false) {
   curl_close($curl);
 }
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
+  <!-- Required meta tags -->
   <meta charset="utf-8">
-  <title>Backup your NFT's</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <title>Backup your Tezos NFT's</title>
 </head>
 
 <body>
-  <p>This page is meant to be used in combination with <a href="https://www.downthemall.net/">DownThemAll</a>. Install
-    the extension and add the following links to the download queue:</p>
-  <?php
-  $amount = count($links);
-  if ($amount > 0) {
-    echo "<p>Found {$amount} tokens in {$address}.</p>";
-    foreach ($links as $link) {
-      echo $link;
+  <div class="container-fluid">
+    <h1>Backup your Tezos NFT's</h1>
+    <p>This page is meant to be used in combination with <a href="https://www.downthemall.net/">DownThemAll</a>. Install
+      the extension and add the following links to the download queue:</p>
+    <?php
+    $amount = count($links);
+    if ($amount > 0) {
+      echo "<p>Found {$amount} tokens in {$address}.</p>";
+      foreach ($links as $link) {
+        echo $link;
+      }
+    } else {
+      echo "<p>This wallet does not contain any tokens.</p>";
     }
-  } else {
-    echo "<p>This wallet does not contain any tokens.</p>";
-  }
-  ?>
+    ?>
+  </div>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
