8cfcc09dff50e9ae90b9e36cf3c89a215b38885c
Yannick Goossens <yannick@ziroh.be>
2024-04-10 18:16:32 +0200

    split sitemap

--- src/sitemap.php
+++ src/sitemap.php
@@ -1,21 +1,21 @@
 <?php
+header('Content-Type: application/xml');
 
-if (file_exists('/tmp/sitemap.xml') && time() - filemtime('/tmp/sitemap.xml') < getenv('REFRESH_INTERVAL')) {
+if (file_exists('/var/www/sitemap/sitemap.xml') && time() - filemtime('/var/www/sitemap/sitemap.xml') < getenv('REFRESH_INTERVAL')) {
     // Sitemap was generated less than 24 hours ago, return its content
-    $sitemap = file_get_contents('/tmp/sitemap.xml');
-    header('Content-Type: application/xml');
+    $sitemap = file_get_contents('/var/www/sitemap/sitemap.xml');
     echo $sitemap;
     exit;
 }
 
 // Create a new DOM document
-$dom = new DOMDocument('1.0', 'UTF-8');
-$dom->formatOutput = true;
+$sitemap = new DOMDocument('1.0', 'UTF-8');
+$sitemap->formatOutput = false;
 
-// Create the root <urlset> element
-$urlset = $dom->createElement('urlset');
-$urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-$dom->appendChild($urlset);
+// Create the root <sitemapindex> element
+$sitemapindex = $sitemap->createElement('sitemapindex');
+$sitemapindex->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+$sitemap->appendChild($sitemapindex);
 
 // Fetch the tokens from the GraphQL endpoint
 $query = 'query sitemap {
@@ -36,18 +36,34 @@ if ($response) {
     $data = json_decode($response, true);
 
     if (isset($data['data']['tokens'])) {
-        // Iterate over the tokens and create <url> elements
-        foreach ($data['data']['tokens'] as $token) {
-            $url = $dom->createElement('url');
+        $total = count($data['data']['tokens']);
+        for($i = 0; $i < $total; $i += 50000) {
+            // Create a new DOM document
+            $dom = new DOMDocument('1.0', 'UTF-8');
+            $dom->formatOutput = false;
 
-            $loc = $dom->createElement('loc', getenv('BASE_URL') . 'objkt/' . $token['token_id']);
-            $url->appendChild($loc);
+            // Create the root <urlset> element
+            $urlset = $dom->createElement('urlset');
+            $urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+            $dom->appendChild($urlset);
 
-            $urlset->appendChild($url);
+            // Iterate over the tokens and create <url> elements
+            foreach ($data['data']['tokens'] as $token) {
+                $url = $dom->createElement('url');
+                $loc = $dom->createElement('loc', getenv('BASE_URL') . '/objkt/' . $token['token_id']);
+                $url->appendChild($loc);
+                $urlset->appendChild($url);
+            }
+            $dom->save('/var/www/sitemap/sitemap_' . $i . '.xml');
+
+            $sub = $sitemap->createElement('sitemap');
+            $loc = $sitemap->createElement('loc', getenv('SITEMAP_HOST') . '/sitemap_' . $i . '.xml');
+            $sub->appendChild($loc);
+            $sitemapindex->appendChild($sub);
         }
     }
 }
 
 // Save the XML document to a file
-$dom->save('/tmp/sitemap.xml');
-echo $dom->saveXML();
+$sitemap->save('/var/www/sitemap/sitemap.xml');
+echo $sitemap->saveXML();
